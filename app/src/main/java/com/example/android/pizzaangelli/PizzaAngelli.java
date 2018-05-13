15d329294d02bb4bff9cd959adcbb56044283d23
SPQRapid <tudor.conciu@yahoo.com>
2018-05-13 08:14:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/pizzaangelli/PizzaAngelli.java
@@ -0,0 +1,14 @@
+package com.example.android.pizzaangelli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PizzaAngelli extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizza_angelli);
+    }
+
+}
